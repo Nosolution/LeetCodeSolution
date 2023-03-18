dd7c4831912916a8ea4eafb4e90c327463b7f317
nosolution <1206985125@qq.com>
2023-03-19 00:44:19 +0800

    1884

---
+++ _1884EggDropWith2EggsAndNFloors.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+/**
+ * 1884. Egg Drop With 2 Eggs and N Floors
+ * Medium
+ * <p>
+ * You are given two identical eggs and you have access to a building with n floors labeled from 1 to n.
+ * You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
+ * In each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If the egg breaks, you can no longer use it. However, if the egg does not break, you may reuse it in future moves.
+ * Return the minimum number of moves that you need to determine with certainty what the value of f is.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 2
+ * Output: 2
+ * Explanation: We can drop the first egg from floor 1 and the second egg from floor 2.
+ * If the first egg breaks, we know that f = 0.
+ * If the second egg breaks but the first egg didn't, we know that f = 1.
+ * Otherwise, if both eggs survive, we know that f = 2.
+ * Example 2:
+ * <p>
+ * Input: n = 100
+ * Output: 14
+ * Explanation: One optimal strategy is:
+ * - Drop the 1st egg at floor 9. If it breaks, we know f is between 0 and 8. Drop the 2nd egg starting from floor 1 and going up one at a time to find f within 8 more drops. Total drops is 1 + 8 = 9.
+ * - If the 1st egg does not break, drop the 1st egg again at floor 22. If it breaks, we know f is between 9 and 21. Drop the 2nd egg starting from floor 10 and going up one at a time to find f within 12 more drops. Total drops is 2 + 12 = 14.
+ * - If the 1st egg does not break again, follow a similar process dropping the 1st egg from floors 34, 45, 55, 64, 72, 79, 85, 90, 94, 97, 99, and 100.
+ * Regardless of the outcome, it takes at most 14 drops to determine f.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 1000
+ */
+public class _1884EggDropWith2EggsAndNFloors {
+
+    public int twoEggDrop(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int k = 2; k <= n; k++) {
+            for (int i = 2; i <= k; i++) {
+                //i-1代表broke后在[1, i-1]区间内最大尝试次数
+                //memo[k-i]代表在[i+1, k]区间内的最大尝试次数
+                memo[k] = Math.min(memo[k], 1 + Math.max(i - 1, memo[k - i]));
+            }
+        }
+
+        return memo[n];
+    }
+
+}
+
+
+
+
